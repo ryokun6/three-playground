93ce67c99ae540137be3dc5a311b5e461c7a9454
Ryo Lu <me@ryo.lu>
2025-01-02 00:21:11 -0800

    aaa

--- api/search.ts
+++ api/search.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export const handler = async (req: VercelRequest, res: VercelResponse) => {
   const { query } = req.query;
 
   const INVIDIOUS_INSTANCES = [
@@ -26,4 +26,6 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   }
 
   return res.status(500).json({ error: "Failed to fetch from all instances" });
-}
+};
+
+export default handler;
